f1f4abb4724214315bde687bc4819b7108dee420
RickSandov <i.s.ricardo.sandoval@gmail.com>
2024-03-31 19:35:16 -0600

    send email logs

--- src/emails/index.ts
+++ src/emails/index.ts
@@ -3,6 +3,23 @@ import { render } from "@react-email/render";
 import { DonationReceiptEmail } from "./receipt-email";
 import { TDonor, TProduct } from "@/types";
 
+const transporter = nodemailer.createTransport({
+  host: process.env.EMAIL_HOST || "",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL || "",
+    pass: process.env.EMAIL_PASSWORD || "",
+    // production
+    // user: "i.s.ricardo.sandoval@gmail.com", // development
+    // pass: "zsxyyrugfktoayzt", // development
+  },
+});
+
+transporter.verify().then(async () => {
+  console.log("ready to send emails");
+});
+
 export async function sendSuccessDonationEmail({
   to,
   donor,
@@ -29,23 +46,8 @@ export async function sendSuccessDonationEmail({
       html: emailHtml,
     };
 
-    const transporter = nodemailer.createTransport({
-      host: process.env.EMAIL_HOST || "",
-      port: 465,
-      secure: true,
-      auth: {
-        user: process.env.EMAIL || "",
-        pass: process.env.EMAIL_PASSWORD || "",
-        // production
-        // user: "i.s.ricardo.sandoval@gmail.com", // development
-        // pass: "zsxyyrugfktoayzt", // development
-      },
-    });
-
-    transporter.verify().then(async () => {
-      const send = await transporter.sendMail(options);
-      console.log({ send });
-    });
+    console.log("sending email");
+    const send = await transporter.sendMail(options);
   } catch (error) {
     console.log("error from email index", { error });
   }
