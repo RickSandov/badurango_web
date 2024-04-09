06c303742b319d552f69d2e25fe70b46e931f676
RickSandov <i.s.ricardo.sandoval@gmail.com>
2024-04-09 14:45:38 -0600

    Oxxo donation verification

--- src/server/handlers/donation.handler.ts
+++ src/server/handlers/donation.handler.ts
@@ -1,9 +1,21 @@
 import { TDonation } from "@/types";
 import { Donation } from "../models";
+import { stripe } from "@/app/api/stripe";
 
 export const getDonationById = async (
   donationId: string
 ): Promise<TDonation> => {
   const donation = await Donation.findById(donationId);
+
+  if (donation.paymentStatus === "requires_action") {
+    stripe.paymentIntents.retrieve(donation.paymentId).then((res) => {
+      const paymentStatus = res.status;
+      if (paymentStatus !== donation.paymentStatus) {
+        donation.paymentStatus = paymentStatus;
+        donation.save();
+      }
+    });
+  }
+
   return donation;
 };
